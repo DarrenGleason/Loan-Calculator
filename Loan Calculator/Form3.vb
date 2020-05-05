5fb6eb1113db8f03d14a641d7c9eee63d0c32e7e
Paul McCourt <paulmcollege123@gmail.com>
2020-05-05 02:34:08 -0600

    Added comment form3

--- Loan Calculator/Form3.vb
+++ Loan Calculator/Form3.vb
@@ -3,7 +3,7 @@
         Form4.Show()
         Me.Hide()
     End Sub
-
+    'Test Comment
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Form4.Show()
         Me.Hide()
