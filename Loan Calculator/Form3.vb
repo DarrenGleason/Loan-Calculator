1ac470b12713317b75807d9a21dd7ec8c37b9c4d
Paul McCourt <paulmcollege123@gmail.com>
2020-05-05 01:50:06 -0600

    Added test comment in Form3.vb

--- Loan Calculator/Form3.vb
+++ Loan Calculator/Form3.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form3
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Form4.Show()
+        'Test comment
         Me.Hide()
     End Sub
 
