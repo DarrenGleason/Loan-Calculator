a20454fcf35c622de56be07d9a26677433673961
Paul McCourt <paulmcollege123@gmail.com>
2020-05-05 19:51:15 -0600

    -Added array of structs implementation called loans -Changed name of struct from loans to loan -Added initem function to initialize struct -Button1 code now inserts into array of structs

--- Loan Calculator/Form2.vb
+++ Loan Calculator/Form2.vb
@@ -1,4 +1,4 @@
-﻿Public Structure loans
+﻿Public Structure loan
     Public name As String
     Public principle As String
     Public interest As Double
@@ -6,7 +6,20 @@
 End Structure
 
 Public Class Form2
+    Dim maxloans = 30
+    Dim loans(maxloans) As loan  'Array of loan structs which contains up to 30 loans
     Dim i As Integer
+
+    Sub initem()  'Initialize loans struct
+        For num As Integer = 0 To maxloans
+            loans(num).name = "N/A"
+            loans(num).principle = "000"
+            loans(num).interest = 0.0
+            loans(num).period = -1
+        Next
+
+    End Sub
+
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Form3.Show()
         Me.Hide()
@@ -18,29 +31,34 @@ Public Class Form2
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Button2.Show()
         Button3.Show()
-        If i = 0 Then
-            Dim first As loans = New loans
-            first.name = TextBox1.Text
-            first.principle = TextBox2.Text
-            first.interest = ComboBox2.Text
-            first.period = ComboBox1.Text
-        End If
-        If i = 1 Then
-            Dim second As loans = New loans
-            second.name = TextBox1.Text
-            second.principle = TextBox2.Text
-            second.interest = ComboBox2.Text
-            second.period = ComboBox1.Text
-        End If
-        If i = 2 Then
-            Dim third As loans = New loans
-            third.name = TextBox1.Text
-            third.principle = TextBox2.Text
-            third.interest = ComboBox2.Text
-            third.period = ComboBox1.Text
-        End If
+
+        loans(i).name = TextBox1.Text
+        loans(i).principle = TextBox2.Text
+        loans(i).principle = ComboBox2.Text
+        loans(i).period = ComboBox1.Text
+
+        'If i = 0 Then
+        'Dim first As loans = New loans
+        'first.name = TextBox1.Text
+        'first.principle = TextBox2.Text
+        'first.interest = ComboBox2.Text
+        'first.period = ComboBox1.Text
+        ' End If
+        ' If i = 1 Then
+        'Dim second As loans = New loans
+        ' Second.name = TextBox1.Text
+        ' Second.principle = TextBox2.Text
+        ' Second.interest = ComboBox2.Text
+        ' Second.period = ComboBox1.Text
+        ' End If
+        'If i = 2 Then
+        'Dim third As loans = New loans
+        ' third.name = TextBox1.Text
+        'third.principle = TextBox2.Text
+        'third.interest = ComboBox2.Text
+        'third.period = ComboBox1.Text
+        ' End If
         i = i + 1
-        'Added another test comment
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -55,6 +73,7 @@ Public Class Form2
     End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        initem() 'Calls initem function above to initialize array of structs
         Button2.Hide()
         Button3.Hide()
         i = 0
